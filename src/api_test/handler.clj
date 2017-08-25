992101ef9277769b89b24cd2a178d5384641f3eb
pcolliander <pontus.colliander@gmail.com>
2017-08-25 13:47:00 +0200

    made sessions work.

--- src/api_test/handler.clj
+++ src/api_test/handler.clj
@@ -1,15 +1,17 @@
 (ns api-test.handler
   (:require [compojure.core :refer :all]
-            [compojure.handler :as handler]
-            [ring.middleware.json :as middleware]
+            [compojure.handler :as compojure-handler]
+            [ring.middleware.json :as json-middleware]
+            [ring.middleware.defaults :refer :all]
+            [ring.middleware.session :refer [wrap-session]]
+
             [buddy.auth.backends :as backends]
             [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
             [buddy.auth.backends.session :refer [session-backend]]
             [buddy.auth.accessrules :refer [restrict]]
             [buddy.auth :refer [authenticated? throw-unauthorized]]
             [compojure.route :as route]
-            )
- )
+            ))
 
 (use 'ring.middleware.session)
 (set! *warn-on-reflection* true)
@@ -17,7 +19,7 @@
 (def credentials 
   {:admin "pw" })
 
-(def backend (backends/session))
+
 (defroutes app-routes
    (GET "/" request 
         (println)
@@ -26,62 +28,64 @@
         (println)
 
         (println (:session request))
+        (let [count (or ((:session request) :count) 0)
+              identity (or ((:session request) :identity) "anonymous") ]
 
-        {:status 200 })
+        {:status 200 :session {:identity identity :count (inc count)}}))
 
    (POST "/login" request 
          (let [username (get-in request [:params :username])
                password (get-in request [:params :password])
                session (:session request)
-               updated-session (assoc session :identity (keyword username))]
+               found-password (credentials (keyword username))]
 
-           (println updated-session)
            (println)
            (println request)
            (println)
            (println "username: " username )
            (println "password: " password )
+           (println "found-password " found-password)
 
-           (if (= password (credentials (keyword username)))
-             {:status 200 :session (assoc request :session updated-session) :body {:desc (str "welcome "  username)}}
+           (if (and found-password (= password found-password))
+             {:status 200 :session (assoc (get-in request [:session]) :identity (keyword username)) :body {:desc (str "welcome "  username)}}
              {:status 401 :body {:desc "you are not authorised"}})))
+  (GET "/logout" request
+       {:status 200 :session nil })
 
    (GET "/protected" request
         (println)
-      (println request)
+      (println "REQUEST: " request)
         (println)
-      (println authenticated? request)
+      (println (authenticated? request))
+      (println "boolean (:session request) " (boolean (:session request) ))
+      ;; (println ":identity (:session request) " (:identity (:session request)))
+        (println ":session request " (:session request))
         (println)
-      (if (authenticated? request)
-        {:status 200 } 
+      (if (authenticated? (:session request))
+        {:status 200 :body {:accounts [1 2 3] } }
         {:status 401 }
       ))
 
   (route/not-found "Not Found"))
 
-(def app
-  (-> 
-    (handler/site app-routes)
-    ;; (wrap-session {:cookie-attrs {:max-age 3600 :secure true}})
-    (wrap-authentication backend)
-    (wrap-authorization backend)
-    (middleware/wrap-json-body {:keywords? true})
-    middleware/wrap-json-response))
+(defn wrap-content-type [handler content-type]
+  (println "yoddliho")
+  (fn [request]
+    (let [response (handler request)]
+      (assoc-in response [:headers "Content-Type"] content-type))))
 
+(def backend (session-backend))
+
+(def app
+  (-> app-routes
+    (wrap-content-type "application/json")
+    (wrap-authentication backend)
+    ;; (wrap-authorization backend)
+    (compojure-handler/site)
+    (json-middleware/wrap-json-body {:keywords? true})
+    json-middleware/wrap-json-response))
+
+; TESTING
 ;; curl -c cookies.txt -X POST -F 'username=admin' -F 'password=pw' "http://localhost:3000/login"
 ;; curl -b cookies.txt -I "http://localhost:3000/protected"
-
-;; (use 'ring.middleware.session
-;;           'ring.util.response)
-;; (use 'ring.middleware.cookies)
-;;
-;; (defn handler [request]
-;;   (let [session [:session request]]
-;;     (println request)
-;;     {:status 200 :cookies {"session_id" {:max-age 3600 :http-only true :value "session-id-hashbrownie"}}}))
-;;
-;; (def app
-;;     (wrap-cookies handler))
-
-
 
